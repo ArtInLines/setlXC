78b26e290afa5276ccee78c4348f1497ae3bf308
ArtInLines <linesinart@gmail.com>
2023-10-08 17:31:34 +0200

    Split src code into separate files

--- comp.bat
+++ comp.bat
@@ -1,5 +1,7 @@
 @echo off
-call setlX setlXC.stlx -p %1.stlx %2
+cd src
+call setlX setlXC.stlx -p ../%1.stlx %2
 @REM Calling setlX turns the echo on again, so we need to turn it off again
 @echo off
+cd ..
 call gcc -g -o out out.c
